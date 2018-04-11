c9300881f591eb5d851afd4074bd777f181c7bc3
Mei <meikonishi@gmail.com>
2018-04-11 17:56:18 +0800

    Fixed collision issue. Simon says plays fine now

--- FullCombinedPrototype/Assets/Scripts/PuzzleManager.cs
+++ FullCombinedPrototype/Assets/Scripts/PuzzleManager.cs
@@ -42,7 +42,7 @@ public class PuzzleManager : MonoBehaviour
         currentActivePuzzle = 1;
 
         puzzleSolvedText = GameObject.Find("PuzzleSolvedText").GetComponent<Text>();
-        Instantiate(puzzleObjects[0], new Vector3(2.19f, -2.65f, 0.05f), Quaternion.identity);
+        Instantiate(puzzleObjects[1], new Vector3(2.19f, -2.65f, 0.05f), Quaternion.identity);
 
         // TESTING putting in different controllers into array of interface
         puzzleControllers = new PuzzleControllerInterface[2];
@@ -53,15 +53,15 @@ public class PuzzleManager : MonoBehaviour
         slottedPuzzleCtrls = new PuzzleControllerInterface[2];
         slottedPuzzleCtrls[0] = puzzleControllers[0];
         slottedPuzzleCtrls[1] = puzzleControllers[1];
-
-    
+        
 
         StartFirstPuzzle();
     }
 
     private void StartFirstPuzzle()
     {
-        slottedPuzzleCtrls[0].Play();
+        slottedPuzzleCtrls[0].Play(); // set the first puzzle to active
+        puzzleObjects[1].SetActive(false);  // set the objects of second puzzle to deactivate
     }
 
     // call this function when atk puzzle is solved!
@@ -144,14 +144,20 @@ public class PuzzleManager : MonoBehaviour
             // activate and deactivate puzzles 
             switch (puzzleNumber)
             {
-                case 1: slottedPuzzleCtrls[0].Play();
-                    slottedPuzzleCtrls[1].Stop();
+                case 1: slottedPuzzleCtrls[0].Play(); // activate puzzle
+                    //puzzleObjects[0].SetActive(true); // activate objects
+                    slottedPuzzleCtrls[1].Stop(); // deactivate puzzles
+                    puzzleObjects[1].SetActive(false); // hide objects
                     break;
                 case 2: slottedPuzzleCtrls[0].Stop();
+                    puzzleObjects[0].SetActive(false);
                     slottedPuzzleCtrls[1].Play();
+                    puzzleObjects[1].SetActive(true);
                     break;
                 case 3: slottedPuzzleCtrls[0].Stop();
+                    puzzleObjects[0].SetActive(false);
                     slottedPuzzleCtrls[1].Stop();
+                    puzzleObjects[1].SetActive(true);
                     break;
             }
         }
