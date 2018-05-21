0edc37dd7255ec770aa60574e5d39e41366b4e25
kaizhong <taykz1990@gmail.com>
2018-05-21 12:02:13 +0800

    Follow The Leader
    Committing FTL game
    No other codes were touched

---
+++ FullCombinedPrototype/Assets/Scripts/FollowTheLeader/FTLCard.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FTLCard : MonoBehaviour {
+
+    [SerializeField]
+    private FollowTheLeaderController controller;
+    private int id;
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void OnMouseDown()
+    {
+        controller.CheckCard(this);
+        if(controller.CheckWin())
+        {
+            controller.SetupCards();
+            controller.StartGame();
+        }
+    }
+
+    public int getID
+    {
+        get { return id; }
+    }
+
+    public void setID(int newID)
+    {
+        id = newID;
+    }
+
+
+}
---
+++ FullCombinedPrototype/Assets/Scripts/FollowTheLeader/FollowTheLeaderController.cs
@@ -0,0 +1,126 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowTheLeaderController : PuzzleControllerInterface
+{
+    // Override Variables
+    public bool IsEnable = false;
+
+    // Variables
+    public int level = 1;
+    public List<FTLCard> cards;
+
+    private bool[] gameStateCheck = new bool[9]; //default is false
+    private List<int> cardList = new List<int>();
+    private Queue<int> cardsToTap = new Queue<int>();   
+    private int cardsLeftToLight = 0;
+
+
+    // Override Functions
+    public override void Start () {
+
+        base.Start();
+        puzzleType = 2; // this puzzle is a defence type
+        puzzleDifficulty = 1; // set puzzle difficulty to 1 first
+
+        //SetupCards();
+        //StartGame();
+    }
+
+    public override void Play()
+    {
+        IsEnable = true;
+        SetupCards();
+        StartGame();
+    }
+
+    public override void Stop()
+    {
+        IsEnable = false;
+        /* base.Stop();
+        stop = true;
+        gameActive = false;*/
+    }
+
+    // Update is called once per frame
+    void Update () {
+
+    }
+
+    // Functions Here
+    public void StartGame()
+    {        
+        // Add the cards to tap to the queue
+        StartCoroutine(WaitForReveal());
+    }
+
+    private IEnumerator WaitForReveal()
+    {
+        int temp;
+        int index;
+        while (cardsLeftToLight > 0)
+        {
+            temp = Random.Range(0, cardList.Count);
+            index = cardList[temp];
+            cardList.RemoveAt(temp);
+            gameStateCheck[index] = true;
+            cardsToTap.Enqueue(index);
+
+            // HOW TO MAKE CARD BECOME ACTIVE
+            cards[index].gameObject.SetActive(true);
+            yield return new WaitForSecondsRealtime(1);
+
+            cardsLeftToLight--;
+        }      
+    }
+
+    public void CheckCard(FTLCard card)
+    {
+        if (card.getID == cardsToTap.Peek())
+        {
+            int cardID = card.getID;
+            cardList.Add(cardID);
+            gameStateCheck[cardID] = false;
+            cardsToTap.Dequeue();
+            cards[cardID].gameObject.SetActive(false);
+        }
+    }
+
+    public bool CheckWin()
+    {
+        if (cardsToTap.Count == 0)
+        {
+            for (int i = 1; i <= gameStateCheck.Length; i++)
+            {
+                if (gameStateCheck[i-1] == true)
+                {
+                    return false;
+                }
+            }
+            // Reset number of cards for next round if true
+            puzzleManagerScript.PuzzleSolved(puzzleType);
+            //SetupCards();
+            //StartGame();
+            return true;
+        } else
+        {
+            return false;
+        }       
+    }
+
+    public void SetupCards()
+    {
+        cardsLeftToLight = level + 2;
+        cardsToTap.Clear();
+        cardList.Clear();
+        for (int i = 0; i < cards.Count; i++)
+        {    
+            gameStateCheck[i] = false;          
+            cards[i].setID(i);
+            cards[i].gameObject.SetActive(false);
+            cardList.Add(i);
+        }
+    }
+
+}
