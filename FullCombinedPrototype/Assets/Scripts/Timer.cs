638805142882c5f9876428c5efde7551a91c32a0
kaizhong <taykz1990@gmail.com>
2018-05-28 20:22:35 +0800

    Attempt 1 at fixing conflict

--- FullCombinedPrototype/Assets/Scripts/Timer.cs
+++ FullCombinedPrototype/Assets/Scripts/Timer.cs
@@ -3,39 +3,48 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class Timer : MonoBehaviour {
+public class Timer : MonoBehaviour
+{
 
     public static Timer instance = null; // Static instance of Timer
-    public float puzzleDuration = 5.0f;
+    public float puzzleDuration;
 
-    private float timeLeft; 
+    private float timeLeft;
     private Text timerText;
+    private float splashScreenDelay = 2.0f;
     private float animationDelay = 2.0f;
 
-    private static bool puzzlePhase;   // true for puzzle phase, false for attack phase
+    private GameManager gameManager;
+
+    private static int phase;   // 1 for puzzle phase, 2 for attack phase, 0 for tutorial, 
+                                // 3 for puzzle splash screen, 4 for attack splash screen
     private static bool playerAtkTurn; // true for player's turn, false for enemy's turn
     private static bool animationTrigger; // true for animation to play (used as trigger switch)
     private static bool readyForNextRound; // true to start next round (used as a trigger switch)
-    private static bool stop; // use for game over
+    private static bool stop; // use for showing tutorial, or game over
 
-	// Use this for initialization
-	void Awake () {
+    // Use this for initialization
+    void Awake()
+    {
 
-        if (instance == null){ // check if instance already exists
+        if (instance == null)
+        { // check if instance already exists
             instance = this;    // if not, set instance to this
         }
 
-        else if (instance != this){ // if instance already exists and is not this
+        else if (instance != this)
+        { // if instance already exists and is not this
             Destroy(gameObject);    // then destroy it. enforcing singleton
         }
 
-        timeLeft = puzzleDuration;
+        timeLeft = splashScreenDelay;
 
         timerText = GameObject.Find("Timer").GetComponent<Text>();
+        gameManager = FindObjectOfType<GameManager>();
 
-        puzzlePhase = true;   
+        phase = 3;
         playerAtkTurn = true;
-        animationTrigger = false;
+        animationTrigger = true;
         readyForNextRound = false;
         stop = false;
     }
@@ -43,7 +52,7 @@ public class Timer : MonoBehaviour {
     public void resetTimer()
     {
         timeLeft = puzzleDuration;
-        puzzlePhase = true;
+        phase = 3;
     }
 
     public static void stopTimer()
@@ -53,7 +62,10 @@ public class Timer : MonoBehaviour {
 
     public static bool checkPuzzlePhase()
     {
-        return puzzlePhase;
+        if (phase == 1)
+            return true;
+        else
+            return false;
     }
 
     public static bool checkPlayerTurn()
@@ -79,7 +91,7 @@ public class Timer : MonoBehaviour {
     // this way animation only plays once
     public static bool checkAnimationTriggered(string name)
     {
-        // do separately for player and enemy...
+        // do separately for all animations...
         // i'm sorry I dunno how to make this neater ToT 
         if (name == "player" && playerAtkTurn)
         {
@@ -105,32 +117,84 @@ public class Timer : MonoBehaviour {
             else return false;
         }
 
+        if (name == "splashPuzzle" && phase == 3)
+        {
+            if (animationTrigger)
+            {
+                animationTrigger = false;
+                return true;
+            }
+            else return false;
+        }
+
+        if (name == "splashAttack" && phase == 4)
+        {
+            if (animationTrigger)
+            {
+                animationTrigger = false;
+                return true;
+            }
+            else return false;
+        }
+
         return false;
     }
 
     // Update is called once per frame
-    void Update() {
+    void Update()
+    {
 
         // so long as timer isn't stopped
         if (!stop)
         {
             // count down timer for puzzle phase
-            if (puzzlePhase)
+            if (phase == 1)
             {
                 timerText.text = "" + ((int)timeLeft + 1);
                 timeLeft -= Time.deltaTime;
-                if (timeLeft <= 0)
+                if (timeLeft <= 0) // when time is up
                 {
-                    puzzlePhase = false;    // enter attack phase
-                    playerAtkTurn = true;   // player's turn first
+                    phase = 4;    // play attack phase splash screen
                     PuzzleManager.StopPuzzle(); // make the puzzles inactive
-                    timeLeft = animationDelay;
-                    animationTrigger = true; // play player's animation
+                    timeLeft = splashScreenDelay;
+                    animationTrigger = true; // play splash screen animation
                 }
             }
 
-            // count down timer for animation delay
-            if (!puzzlePhase)
+            // count down timer for animation delays ===================================
+
+            if (phase == 3) // puzzle splash screen animation
+            {
+                timeLeft -= Time.deltaTime;
+                if (timeLeft <= 0) // when time is up
+                {
+                    // if player has never played game before
+                    if (PlayerPrefs.GetInt("tutorial") == 0)
+                    {
+                        // stop timer and show tutorial screen
+                        stop = true;
+                        PlayerPrefs.SetInt("tutorial", 1); // set tutorial seen, go to next
+                        gameManager.showTutorial(1);
+                    }
+                    else
+                    {
+                        startPuzzle();
+                    }
+                }
+            }
+
+            if (phase == 4) // attack splash screen animation
+            {
+                timeLeft -= Time.deltaTime;
+                if (timeLeft <= 0) // when time is up
+                {
+                    phase = 2; // go into attack mode
+                    timeLeft = animationDelay;
+                    animationTrigger = true; // play character attack animations
+                }
+            }
+
+            if (phase == 2) // characters attacking animation
             {
                 // count down for player's animation delay
                 if (playerAtkTurn)
@@ -152,19 +216,30 @@ public class Timer : MonoBehaviour {
                     timeLeft -= Time.deltaTime;
                     if (timeLeft <= 0)  // enemy's turn ends
                     {
-                        readyForNextRound = true; // tell timer that ready for next round to start
-                        puzzlePhase = true; // go back to puzzle mode
                         playerAtkTurn = true; // set back to player's turn for next round
-                        PuzzleManager.StartPuzzle(); // activate the puzzle again
-                        timeLeft = puzzleDuration; // set timer back to puzzle count down
+                        phase = 3; // go to play puzzle splash screen 
+                        timeLeft = splashScreenDelay;
+                        animationTrigger = true;
                     }
                 }
             }
+
         }
 
         if (stop)
         {
-            timerText.text = "GAME OVER";
+            //  timerText.text = "";
         }
+    }
+
+    // function that does all the set up to prepare to start puzzle mode
+    public void startPuzzle()
+    {
+        stop = false;
+        phase = 1; // go into puzzle mode
+        readyForNextRound = true; // tell timer that ready for puzzle round to start
+        PuzzleManager.StartPuzzle(); // activate the puzzle again
+        timeLeft = puzzleDuration; // set timer back to puzzle count down
+        animationTrigger = true; // play character attack animations
     }
 }
